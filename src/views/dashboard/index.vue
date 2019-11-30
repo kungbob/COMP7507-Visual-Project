db8bd47c9c20efcb9b28b3ca655719342d1a60b7
kung <kungtszho@gmail.com>
2019-11-30 08:06:44 +0800

    Modify Dashboard

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">Welcome to 2020 Legislative Council Election Visualization Platform!</div>
+    <p><b>Group 29: </b><u>Candidate Comparison and Recommendation Visualization Tool</u></p>
+    <p>Please find your geographical constituency in the map below. After that, you can click the tabs on the left to <br>
+      <ul>
+      <li>Read the candidates' information; </li>
+      <li>Compare the candidates in the same geographical constituency; </li>
+      <li>Answer the questionnaire and then our system will recommend a candidate for you.</li>
+      </ul>
+    </p>
+    <iframe id="iframe" src="Tableau/Dashboard.html"></iframe>
   </div>
 </template>
 
@@ -13,6 +22,13 @@ export default {
     ...mapGetters([
       'name'
     ])
+  },
+  mounted() {
+    const oIframe = document.getElementById("iframe");
+    const deviceWidth = document.documentElement.clientWidth;
+    const deviceHeight = document.documentElement.clientHeight;
+    oIframe.style.width = Number(deviceWidth) - 220 + "px";
+    oIframe.style.height = Number(deviceHeight) - 120 + "px";
   }
 }
 </script>
